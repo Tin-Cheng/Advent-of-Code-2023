2dc27f9f07a9786c124e0e13705a8f47de0186b6
Tin <chenghotin@gmail.com>
2023-12-01 18:00:12 +0000

    modify mapping iterator

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,3 +1,5 @@
+
+use std::iter::zip;
 fn main() {
     println!("part2!");
     let input = include_str!("./input.txt");
@@ -5,22 +7,19 @@ fn main() {
     dbg!(output);
 }
 
+const LITERALS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+//BAD METHOD, but works for my input
+const DIGITS: [&str; 9] = ["o1e", "t2", "t3e", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e"];
+
+
 fn placeStringToDigit(mut input: &str) -> String{
-    let mut MAPPIING: Vec<(String,String)> = Vec::new();
-    //BAD METHOD
-    MAPPIING.push((String::from("one"),String::from("o1e")));
-    MAPPIING.push((String::from("two"),String::from("t2")));
-    MAPPIING.push((String::from("three"),String::from("t3e")));
-    MAPPIING.push((String::from("four"),String::from("f4r")));
-    MAPPIING.push((String::from("five"),String::from("f5e")));
-    MAPPIING.push((String::from("six"),String::from("s6x")));
-    MAPPIING.push((String::from("seven"),String::from("s7n")));
-    MAPPIING.push((String::from("eight"),String::from("e8t")));
-    MAPPIING.push((String::from("nine"),String::from("n9e")));
     let mut output= String::from(input);
-    for (from,to) in MAPPIING {
-        output = output.replace(&String::from(from), &String::from(to));
-    }
+    let map= zip(LITERALS.iter(), DIGITS.iter());    
+    for (from,to) in map{
+        output = output.replace(from,to);
+    };
     println!("{} : {} ",input,output);
     output
 }
