8cea458c028ac1484bec2f7c2c01112884e77ea9
Tin <chenghotin@gmail.com>
2023-12-09 11:43:13 +0000

    day-09

---
+++ day-09/src/bin/part2.rs
@@ -0,0 +1,62 @@
+fn main() {
+    println!("part2!");
+    let input = include_str!("./input.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(input: &str) -> i64 {
+    let sequences: Vec<Vec<i64>> = input
+        .split('\n')
+        .map(|line| {
+            line.split(' ')
+                .map(|i| {
+                    i.parse::<i64>()
+                        .expect("failed to convert string to integer")
+                })
+                .collect::<Vec<i64>>()
+        })
+        .collect();
+
+    //println!("sequences {:?}",sequences);
+    let mut result = 0;
+
+    for sequence in sequences {
+        let mut differences: Vec<Vec<i64>> = Vec::new();
+        differences.push(sequence.clone());
+        let mut all_zeros: bool = false;
+        while !all_zeros {
+            all_zeros = true;
+            let mut difference: Vec<i64> = Vec::new();
+            for i in 1..differences.last().unwrap().len() {
+                difference
+                    .push(differences.last().unwrap()[i] - differences.last().unwrap()[i - 1]);
+                if *difference.last().unwrap() != 0_i64 {
+                    all_zeros = false;
+                }
+            }
+            differences.push(difference);
+        }
+        let mut next: i64 = 0;
+        for i in (0..differences.len()).rev() {
+            next = differences[i][0] - next;
+        }
+        result += next;
+        //println!("s {:?}, n {}",sequence,next);
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let test_case = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        let result = part2(test_case);
+        assert_eq!(result, 2);
+    }
+}
